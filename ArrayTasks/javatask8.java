b42121916b2466372055ef5c867481538530a683
vani-19 <annavaniyadav19@gmail.com>
2024-12-06 18:09:49 +0530

    Add files via upload

---
+++ ArrayTasks/javatask8.java
@@ -0,0 +1,14 @@
+8. Print Array even positions in reverse order
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        // Loop through the array and print elements at even positions in reverse order
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i % 2 == 1) {  // Check if the index is even
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
